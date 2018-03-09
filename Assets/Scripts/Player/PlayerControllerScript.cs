01b3d5b3e59a89b359a85afe8df5fbd2d46e2040
TaMcC <taylor.mccallum@hotmail.com>
2018-03-09 18:07:05 -0500

    testing git bash

---
+++ Assets/Scripts/Player/PlayerControllerScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+    private Animator playerAnimator;   // player animater
+    private Rigidbody2D playerRigidbody;
+    private SpriteRenderer playerSpriteRenderer;
+
+    public float maxSpeed = 5f;
+    public float jumpForce = 200;
+    public Transform groundCheck;
+    public LayerMask defineGround;  // defined layer, like Floor
+
+    private float speed, vSpeed;
+    private bool isOnGround = false;
+    private float groundRadius = 0.1f; 
+
+
+    // Use this for initialization
+    void Start () {
+        playerAnimator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        //Debug.Log("Grounded? " + isOnGround/* + " Groundcheck: " + groundCheck*/);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetAxis("Jump") > 0 && isOnGround && vSpeed == 0)
+        //{
+        //    isOnGround = false;
+        //    playerAnimator.SetBool("isOnGround", isOnGround);
+        //    playerRigidbody.AddForce(new Vector2(0, jumpForce));
+        //    Debug.Log("Grounded? " + isOnGround/* + " Groundcheck: " + groundCheck*/);
+
+        //}
+        //else
+        //{
+        //    isOnGround = true;
+        //    playerAnimator.SetBool("isOnGround", isOnGround);
+        //}
+    }
+    void FixedUpdate()
+    {
+        isOnGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, defineGround);
+        Debug.Log("Grounded? " + isOnGround/* + " Groundcheck: " + groundCheck*/);
+
+        if (Input.GetAxis("Jump") > 0 && isOnGround && vSpeed == 0)
+        {
+            isOnGround = false;
+            playerAnimator.SetBool("isOnGround", isOnGround);
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+        //else
+        //{
+        //    isOnGround = true;
+        //    playerAnimator.SetBool("isOnGround", isOnGround);
+        //}
+
+        playerAnimator.SetBool("isOnGound", isOnGround);
+        playerAnimator.SetFloat("vSpeed", playerRigidbody.velocity.y);
+        float moveHoriz = Input.GetAxis("Horizontal");
+        playerAnimator.SetFloat("speed", Mathf.Abs(moveHoriz));
+
+        playerRigidbody.velocity = new Vector2(moveHoriz * maxSpeed, playerRigidbody.velocity.y);
+
+        if (moveHoriz > 0)
+        {
+            playerSpriteRenderer.flipX = false;
+        }
+        else if (moveHoriz < 0)
+        {
+            playerSpriteRenderer.flipX = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBScript : MonoBehaviour {
+
+    private Animator animator;
+    private Rigidbody2D rBody;
+    private SpriteRenderer rRend;
+
+    public float maxSpeed = 5f;
+    public float jumpForce = 30;
+    public Transform groundCheck;
+    public LayerMask defineGround;  // defined layer, like Floor
+
+    public int inputChannel = 0;
+
+    private float speed, vSpeed;
+    private bool isOnGround = false;
+    private float groundRadius = 1f;
+
+    
+    // Use this for initialization
+    void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+        rRend = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        //isOnGround = false;
+        //Debug.Log("Grounded? " + isOnGround/* + " Groundcheck: " + groundCheck*/);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetAxis("Jump") > 0 && isOnGround)
+        //{
+        //    isOnGround = false;
+        //    animator.SetBool("isOnGround", isOnGround);
+        //    rBody.AddForce(new Vector2(0, jumpForce));
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        isOnGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, defineGround);
+        animator.SetBool("isOnGound", isOnGround);
+       //Debug.Log("Grounded? " + isOnGround + " Groundcheck: " + groundCheck);
+
+        animator.SetFloat("vSpeed", rBody.velocity.y);
+        float moveHoriz = Input.GetAxis("Horizontal");
+        //switch (inputChannel)
+        //{
+        //    case 1:
+        //        moveHoriz = Input.GetAxis("Horizontal");
+        //        break;
+        //    case 2:
+        //        moveHoriz = Input.GetAxis("Horizontal2");
+        //        break;
+        //    case 3:
+        //        moveHoriz = Input.GetAxis("Horizontal3");
+        //        break;
+        //    default:
+        //        break;
+        //}
+
+        //Debug.Log("isOnGround=" + isOnGround + " speed=" + Mathf.Abs(moveHoriz));
+
+        animator.SetFloat("speed", Mathf.Abs(moveHoriz));
+        rBody.velocity = new Vector2(moveHoriz * maxSpeed, rBody.velocity.y);
+
+        if (moveHoriz > 0)
+        {
+            rRend.flipX = false;
+        }
+        else if (moveHoriz < 0)
+        {
+            rRend.flipX = true;
+        }
+    }
+}
