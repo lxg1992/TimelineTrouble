e26e2565ea794a2e539b34a45a778500658db230
Alex <lxg1992@gmail.com>
2018-03-27 16:06:01 -0400

    Ice Level + Sprites + Scripts

---
+++ Assets/Scripts/IceScripts/FollowPath.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+	public enum FollowType{
+		MoveTowards,
+		Lerp
+	}
+
+	public FollowType Type = FollowType.MoveTowards;
+	public PathOfIceBall path;
+	public float speed = 1;
+	public float maxDistanceToGoal = .1f;
+
+	private IEnumerator<Transform> _currentPoint;
+
+
+	public void Start()
+	{
+		if (path == null) {
+			Debug.LogError ("Path can't be null", gameObject);
+			return;
+		}
+
+		_currentPoint = path.GetPathsEnumerator ();
+		_currentPoint.MoveNext ();
+
+		if (_currentPoint.Current == null) {
+			return;
+		}
+
+		transform.position = _currentPoint.Current.position;
+	}
+
+	public void Update(){
+		if (_currentPoint == null || _currentPoint.Current == null) {
+			return;
+		}
+
+		if (Type == FollowType.MoveTowards) {
+			transform.position = Vector3.MoveTowards (transform.position, _currentPoint.Current.position, Time.deltaTime * speed);
+		} else if(Type == FollowType.Lerp){
+			transform.position = Vector3.Lerp (transform.position, _currentPoint.Current.position, Time.deltaTime * speed);
+
+		}
+
+		var distanceSquared = (transform.position - _currentPoint.Current.position).sqrMagnitude;
+		if (distanceSquared < maxDistanceToGoal * maxDistanceToGoal)
+			_currentPoint.MoveNext ();
+	}
+}
---
+++ Assets/Scripts/IceScripts/PathOfIceBall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PathOfIceBall : MonoBehaviour {
+
+	public Transform[] Points;
+
+	public IEnumerator<Transform> GetPathsEnumerator(){
+		if (Points == null || Points.Length < 1) {
+			yield break;
+		}
+
+		var direction = 1;
+		var index = 0;
+		while (true) {
+			yield return Points [index];
+
+			if (Points.Length == 1)
+				continue;
+
+			if (index <= 0) {
+				direction = 1;
+			} else if (index >= Points.Length - 1) {
+				direction = -1;
+			}
+
+			index = index + direction;
+		}
+	}
+
+	public void OnDrawGizmos(){
+		if (Points == null || Points.Length < 2)
+			return;
+
+		for (int i = 1; i < Points.Length; i++) {
+			Gizmos.DrawLine (Points [i - 1].position, Points [i].position);
+		}
+	}
+}
