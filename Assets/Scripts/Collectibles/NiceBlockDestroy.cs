947d592a093c22447ec45964e36317a3da9a546f
johnalanc <johnalan.calma@gmail.com>
2018-04-01 01:52:45 -0400

    added destroy script in the Nice Blocks

---
+++ Assets/Scripts/Collectibles/NiceBlockDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NiceBlockDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
