ed68d468ca2b16b20c6e0c8887d2f9f672573e51
johnalanc <johnalan.calma@gmail.com>
2018-04-21 10:25:30 -0400

    Global(on all levels) ScoreSheet Added

---
+++ Assets/ScoreSheet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class ScoreSheet : MonoBehaviour {
+
+    private int  gameScore = 0;
+
+	// Use this for initialization
+	void Start()
+    {
+       
+    }
+
+    //// Update is called once per frame
+    //void Update () {
+    //}
+
+    
+    private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            gameScore = 0;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+
+    public int AddScore(int value)
+    {
+        return gameScore += value;
+    }
+
+    public int GetScore()
+    {
+        return this.gameScore;
+    }
+}
--- Assets/Scripts/Collectibles/Collectibles.cs
+++ Assets/Scripts/Collectibles/Collectibles.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour {
 
+    public GameObject globals;
+    private ScoreSheet scoreSheet;
+    public int scoreValue ;
     private bool touch = false;
     private Animator animController;
     private AudioSource collectSFX;
@@ -12,10 +15,11 @@ public class Collectibles : MonoBehaviour {
 	void Start () {
         collectSFX = GetComponent<AudioSource>();
         animController = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        scoreSheet = globals.GetComponent<ScoreSheet>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (touch && collectSFX.isPlaying == false)
         {
             Destroy(gameObject);
@@ -26,6 +30,8 @@ public class Collectibles : MonoBehaviour {
     {
         if (target.gameObject.tag == "Player" && !touch)
         {
+            scoreSheet.AddScore(this.scoreValue);
+            Debug.Log("current score is :" + scoreSheet.GetScore());
             touch = true;
             animController.SetBool("collected", touch);
             collectSFX.Play();
